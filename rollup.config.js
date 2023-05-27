ecaf41141c621cbca899ff0399c20e97ed7d4da8
André Ciornavei <andre.ciornavei@gmail.com>
2023-05-27 13:07:41 -0300

    build lib as es module

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,19 @@
-import { babel } from "@rollup/plugin-babel";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import json from "@rollup/plugin-json";
-import fs from "fs";
+import { babel } from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
+import fs from 'fs';
 
-const pkg = JSON.parse(fs.readFileSync("./package.json"));
-const external = Object.keys(pkg.dependencies || {}).concat(["fs/promises"]);
+const pkg = JSON.parse(fs.readFileSync('./package.json'));
+const external = Object.keys(pkg.dependencies || {}).concat(['fs/promises']);
 
-const extensions = [".js", ".ts"];
+const extensions = ['.js', '.ts'];
 
 export default {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: {
-    file: "./bin/cli.js",
-    format: "cjs",
-    banner: "#!/usr/bin/env node",
+    file: './bin/cli.js',
+    format: 'es',
+    banner: '#!/usr/bin/env node',
   },
   external,
   plugins: [
@@ -23,8 +23,8 @@ export default {
     }),
     json(),
     babel({
-      exclude: ["node_modules/**", "./history/**"],
-      babelHelpers: "bundled",
+      exclude: ['node_modules/**', './history/**'],
+      babelHelpers: 'bundled',
       extensions,
     }),
   ],
